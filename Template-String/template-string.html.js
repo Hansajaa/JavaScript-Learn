ca5d7504e8903b64f29cb19b6ca4b67ba44ee906
Hansajaa <nilanahansaja21@gmail.com>
2024-02-27 22:20:34 +0530

    template string

---
+++ Template-String/template-string.html.js
@@ -0,0 +1,22 @@
+// let userName = "Nilana";
+
+// console.log(`Welcome ${userName}`);
+
+// console.log(`This is ${24 * 60 * 60 * 365} seconds of a year`);
+
+
+// function discount(value) {
+//     return value * .1;
+// }
+
+// console.log(`Discount is Rs.${discount(1500)}`);
+
+
+document.querySelector('.service').innerHTML=
+`<h2>Our Services</h2>
+<ul>
+    <li>UI & UX Designs</li>
+    <li>Web Development</li>
+    <li>Mobile Apps</li>
+    <li>IT Consultant</li>
+</ul>`
