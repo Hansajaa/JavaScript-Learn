535b049adfd86e868e25b25098ad709d58b53a1d
Hansajaa <nilanahansaja21@gmail.com>
2024-02-27 20:57:21 +0530

    variables

---
+++ variables/variables.js
@@ -0,0 +1,40 @@
+// "var" is global scope
+
+// var vehicle="Honda";
+
+// console.log(vehicle);
+
+// if(true){
+//     var vehicle ="Toyota";
+// }
+
+// console.log(vehicle);
+
+
+// const vehicle="Honda";
+
+// if(true){
+//     const vehicle="Toyota";
+// }
+
+// console.log(vehicle);
+
+
+// "let" keyword is block Scope
+// let vehicle="Honda";
+
+//     if (true) {
+//         let vehicle="Toyota";
+//     }
+
+// console.log(vehicle);
+
+var varCar="honda";
+var varCar="toyota";
+
+// "let" cannot redeclare
+
+// let letCar="honda";
+// let letCar="toyota";
+
+
