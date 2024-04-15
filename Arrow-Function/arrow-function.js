cb4507ca91e83d4959ac9fdb27011c3a91d95672
Hansajaa <nilanahansaja21@gmail.com>
2024-04-16 01:30:12 +0530

    arrow function

---
+++ Arrow-Function/arrow-function.js
@@ -0,0 +1,22 @@
+// function add(a,b){
+//     return a+b;
+// }
+
+// const add = (a,b) => {
+//     return a+b;
+// }
+
+// const devide = (b,c) =>{
+//     return b/c;
+// }
+
+// const add = (a,b) => a+b;
+// const multiply = x => x*x;
+
+// const multiply = x =>  x * x;
+
+// console.log(multiply(2));
+
+// (() => { 
+//     console.log("This is self invoke function");
+// })()
--- Template-String/template-string.html.js
+++ Template-String/template-string.html.js
@@ -4,19 +4,18 @@
 
 // console.log(`This is ${24 * 60 * 60 * 365} seconds of a year`);
 
-
 // function discount(value) {
 //     return value * .1;
 // }
 
 // console.log(`Discount is Rs.${discount(1500)}`);
 
-
-document.querySelector('.service').innerHTML=
-`<h2>Our Services</h2>
-<ul>
-    <li>UI & UX Designs</li>
-    <li>Web Development</li>
-    <li>Mobile Apps</li>
-    <li>IT Consultant</li>
-</ul>`
+document.querySelector(".service").innerHTML = `
+    <h2>Our Services</h2>
+      <ul>
+        <li>UI & UX Designs</li>
+        <li>Web Development</li>
+        <li>Mobile Apps</li>
+        <li>IT Consultant</li>
+        <li>SEO</li>
+      </ul>`;
